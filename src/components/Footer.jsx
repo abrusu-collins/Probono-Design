e9ecd3528561acbd6cd60a45b78b52871c5635de
abrusu-collins <snillochemingson@gmail.com>
2022-08-18 23:53:34 +0000

    Done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,12 @@
+import footerimg from "../img/footer-img.png";
 function Footer() {
-    return ( 
-    <div className="footer">
-
-    </div> 
-    );
+  return (
+    <footer className="footer">
+      <p>
+        Powered By <img src={footerimg} alt="footer-img" />
+      </p>
+    </footer>
+  );
 }
 
 export default Footer;
