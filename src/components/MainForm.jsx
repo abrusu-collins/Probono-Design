c0affef3ce9db77dccb771f236212afe217f2991
abrusu-collins <snillochemingson@gmail.com>
2022-08-19 00:37:19 +0000

    Done

--- src/components/MainForm.jsx
+++ src/components/MainForm.jsx
@@ -4,28 +4,28 @@ function MainForm() {
             <div className="checkboxes">
             <div>
                         <input type="checkbox" name="administration_and_strategy" id="administration_and_strategy"></input>
-                        <label for="administration_and_strategy">ADMIISTRATION & STRATEGY</label>
+                        <label htmlFor="administration_and_strategy">ADMIISTRATION & STRATEGY</label>
                     </div>
                     <div>
                         <input type="checkbox" name="creativity_and_design" id="creativity_and_design"></input>
-                        <label for="creativity_and_design">CREATIVITY & DESIGN</label>
+                        <label htmlFor="creativity_and_design">CREATIVITY & DESIGN</label>
                     </div>
                     <div>
                         <input type="checkbox" name="coaching_and_mentoring" id="coaching_and_mentoring"></input>
-                        <label for="coaching_and_mentoring">COACHING & MENTORING</label>
+                        <label htmlFor="coaching_and_mentoring">COACHING & MENTORING</label>
                     </div>
                     <div>
                         <input type="checkbox" name="marketing_and_fundraising" id="marketing_and_fundraising"></input>
-                        <label for="marketing_and_fundraising">MARKETING & FUNDRAISING</label>
+                        <label htmlFor="marketing_and_fundraising">MARKETING & FUNDRAISING</label>
                     </div>
                     <div>
                         <input type="checkbox" name="technology_and_digital" id="technology_and_digital"></input>
-                        <label for="technology_and_digital">TECHNOLOGY & DIGITAL</label>
+                        <label htmlFor="technology_and_digital">TECHNOLOGY & DIGITAL</label>
                     </div>
 
                     <div>
                         <input type="checkbox" name="all" id="all"></input>
-                        <label for="all">ALL</label>
+                        <label htmlFor="all">ALL</label>
                     </div>
             </div>
             <div className="textinputandsubmitbutton">
